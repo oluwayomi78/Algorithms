6129da2d314f8eb1d3b992524a8dbe87a541b075
preciousenoch <preciousenoch459@gmail.com>
2025-04-07 07:42:53 +0100

    conditional

---
+++ script.js
@@ -0,0 +1,37 @@
+// Lagos Kitchen
+// var food = 'Rice and Beef'
+
+// Mary
+
+
+
+var restaurant = false;
+var food;
+var drinks;
+var beef;
+
+if (restaurant) {
+    if (food == 'Pounded yam and Egusi') {
+        console.log('Buy 5 wraps and 2 big meats');
+    }
+    else if (food == 'Rice and Beef') {
+        console.log('Buy 3 spoon  and plantain with beef');
+    }
+    else if (food == 'Amala and Abula') {
+        console.log('Buy 5 Wraps and 3 big meats');
+    }
+    else {
+        console.log('Sha buy me anything you see');
+    }
+}
+else {
+    if (drinks == 'Cold bottle water') {
+        console.log('Buy 1 bottle and 2 pie');
+    }
+    else if (drinks == 'Nutrimilk') {
+        console.log('buy 1 bottle and 1 pie');
+    }
+    else {
+        console.log('Start coming home');
+    }
+}
